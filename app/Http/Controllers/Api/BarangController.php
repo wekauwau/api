3466740e2a78a09f383765f1f2396bd575ec3dff
wekauwau <wekauwau@gmail.com>
2023-06-07 19:05:04 +0700

    Add Barang api resource

---
+++ app/Http/Controllers/Api/BarangController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BarangResource;
+use App\Models\Barang;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        //get posts
+        // $posts = Barang::latest()->paginate(5);
+        $posts = Barang::all();
+
+        //return collection of posts as a resource
+        return new BarangResource(true, 'List Barang', $posts);
+    }
+}
